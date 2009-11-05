370fd56f798c7899e241685265c4f6c17a87243d
rggibson@ualberta.ca <rggibson@ualberta.ca@c0dba48e-a613-11de-9ca8-173d21a9ba08>
2009-11-05 23:02:28 +0000

    A number of changes here: - Changed MaxN-MC into MaxN-UCT - Added more possible opponent models for KthBestPickDrafter - Added the evaluation function I'm using to play Fantasy Risk - Added GreedyDrafter, which looks at the pick that will give the biggest gain with just a one-step lookahead (ignoring future picks) - Added RandomDrafter, who can draft randomly and can record draft scenarios for machine learning purposes - SmartDrafter has become more bloated with a bunch of utility-type methods... sorry but I don't know where else to put these so that multiple agents can use them.

---
+++ src/com/sillysoft/lux/agent/MaxN_UCT_Drafter.java
@@ -0,0 +1,166 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sillysoft.lux.agent;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Richard Gibson, Neesha Desai, Richard Zhao
+ */
+public class MaxN_UCT_Drafter extends SmartDrafter
+{
+    /**
+     * The maximum number of leaves we want to have on our MaxN portion of the
+     * search
+     */
+    private final int MAX_NUM_LEAVES = 1;
+
+    /**
+     * The number of Monte Carlo roll outs that we average over in MaxNMC, for
+     * each leaf node of the MaxN portion of the search.
+     */
+    private final int NUM_MC_ROLL_OUTS = 3000;
+
+    /**
+     * Should we use the selfish evaluation function?
+     */
+    private final boolean SELFISH = false;
+
+    /**
+     * A cap on how big the branching factor can be.  When there are more branches,
+     * we use the passed in heuristic to reduce the branching down to our max.
+     */
+    private final int MAX_BRANCHING_FACTOR = Integer.MAX_VALUE;
+
+    /**
+     * Constructor
+     */
+    public MaxN_UCT_Drafter()
+    {
+
+    }
+
+    @Override
+    public String name()
+    {
+        if (SELFISH)
+        {
+            return "MaxN_UCT_Drafter-Selfish";
+        }
+        else
+        {
+            return "MaxN_UCT_Drafter";
+        }
+    }
+
+    /**
+     * Calls the heuristic function to get the value of the passed in territory
+     * @param terr The territory whose value we want
+     * @param draftState The state of the draft
+     * @param unownedCountries The countries still available to be picked
+     * @param activePlayer The player whose turn it is to pick
+     * @return The value of the passed in territory
+     */
+//    @Override
+//    protected double getValueOfTerr(int terr, int[] draftState, ArrayList<Integer> unownedCountries, int activePlayer)
+//    {
+//        // Call the heuristic function to get the value of the territory
+//        ArrayList<Integer> index = getTerritoryIndex(terr, draftState, activePlayer);
+//        assert(m_learnedHeuristic != null);
+//        assert(m_learnedHeuristic[terr].containsKey(index));
+//        double[] value = m_learnedHeuristic[terr].get(index);
+//        return value[0];
+//    }
+
+    protected int getPick(int[] draftState, ArrayList<Integer> unownedCountries)
+    {
+        // First, clean up the uct tree
+        cleanUpUctTree(draftState, ID);
+
+        // Determine the depth to which we can search
+        int depth = calculateMaxNSearchDepth(unownedCountries.size(), MAX_BRANCHING_FACTOR, MAX_NUM_LEAVES);
+
+        // Determine which territories to consider picking
+        ArrayList<Integer> picksToConsider = new ArrayList<Integer>();
+        if (MAX_BRANCHING_FACTOR < unownedCountries.size())
+        {
+            int[] picks = getTopPicks(draftState, unownedCountries, ID, MAX_BRANCHING_FACTOR, SELFISH);
+            picksToConsider = new ArrayList<Integer>(picks.length);
+            for (int i = 0; i < picks.length; ++i)
+            {
+                picksToConsider.add(picks[i]);
+            }
+        }
+        else
+        {
+            for (int pick : unownedCountries)
+            {
+                picksToConsider.add(pick);
+            }
+        }
+
+        // Determine which territory to pick
+        int bestPick = -1;
+        double valueOfBestPick = Double.MIN_VALUE;
+        if (depth <= 0)
+        {
+            // Just run UCT right away from the current state
+            maxN_Uct(draftState, unownedCountries, ID, depth, MAX_BRANCHING_FACTOR, NUM_MC_ROLL_OUTS, SELFISH);
+
+            ArrayList<Integer> state = new ArrayList<Integer>(draftState.length);
+            for (int i = 0; i < draftState.length; ++i)
+            {
+                state.add(draftState[i]);
+            }
+
+            // Check all the children to see which is best
+            for (int terr : unownedCountries)
+            {
+                assert(state.get(terr) == -1);
+                state.set(terr, ID);
+                assert(m_uctTree.containsKey(state));
+                double valueOfTerr = (m_uctTree.get(state))[ID + 1]; // First index is reserved for num visits
+                state.set(terr, -1);
+                if (valueOfTerr > valueOfBestPick)
+                {
+                    bestPick = terr;
+                    valueOfBestPick = valueOfTerr;
+                }
+            }
+        }
+        else
+        {
+            for (int countryIndex : picksToConsider)
+            {
+                // Determine the value of picking this country
+                assert(unownedCountries.contains((Integer) countryIndex));
+                assert(unownedCountries.contains((Integer) countryIndex));
+                draftState[countryIndex] = ID; // Pick country
+                unownedCountries.remove((Integer) countryIndex);
+
+                double[] values = maxN_Uct(draftState, unownedCountries, (ID + 1) % board.getNumberOfPlayers(), depth - 1, MAX_BRANCHING_FACTOR, NUM_MC_ROLL_OUTS, SELFISH);
+
+                assert(draftState[countryIndex] != -1);
+                assert(!unownedCountries.contains((Integer) countryIndex));
+                draftState[countryIndex] = -1; // Undo pick
+                unownedCountries.add(countryIndex);
+
+                // Is country better than the rest we've seen?
+                // For now, we break ties by choosing the first country we inspected
+                assert(values.length > ID && ID >= 0);
+                if (values[ID] > valueOfBestPick)
+                {
+                    // Found new best pick
+                    bestPick = countryIndex;
+                    valueOfBestPick = values[ID];
+                }
+            }
+        }
+
+        return bestPick;
+    }
+}
---
+++ src/com/sillysoft/lux/agent/RandomDrafter.java
@@ -0,0 +1,307 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sillysoft.lux.agent;
+
+import com.sillysoft.lux.*;
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+/**
+ * This drafter simply picks territories at random.  His main purpose is to
+ * collect data so that we can stick it in to Weka and get an evaluation
+ * (or objective) function for the draft.
+ * @author Richard Gibson, Neesha Desai, and Richard Zhao
+ */
+public class RandomDrafter extends SmartDrafter
+{
+    /**
+     * The file to store the heuristic function.
+     */
+    protected static String DATA_FILENAME = "C:\\Users\\Richard\\Documents\\NetBeansProjects\\LuxSDK\\LuxSDK\\objectData\\randomDraftData6.arff";
+
+    /**
+     * Will remember the final state of the draft so that when the full game
+     * is over, we can label the data points appropriately as eventual wins or
+     * losses.
+     */
+    private static int[][] finalDraftStates;
+
+    /**
+     * After every this many games, we save the data to file
+     */
+    private static final int SAVE_AFTER_EVERY = 250;
+
+    /**
+     * The maximum number of games of data we want to save (so that the file
+     * doesn't explode in size)
+     */
+    private static final int MAX_NUM_GAMES = 0;
+
+    /**
+     * Continent which we want to reserve some picks for player 0
+     */
+    private static final int RESERVED_CONTINENT = -1; // Asia
+
+    /**
+     * Exact number of territories we want player 0 to own from the above cont
+     */
+     private static final int NUM_RESERVED_TERRS = 0;
+     
+     /**
+      * The reserved territories for player 0
+      */
+     private static ArrayList<Integer> reservedTerrs;
+     
+     /**
+      * The territories which player 0 is not allowed to pick from
+      */
+     private static ArrayList<Integer> antiReservedTerrs;
+
+    /**
+     * Constructor
+     */
+    public RandomDrafter()
+    {
+        super();
+    }
+
+    @Override
+    public String name()
+    {
+        return "RandomDrafter";
+    }
+
+    @Override
+    public void setPrefs(int ID, Board board )
+    {
+        super.setPrefs(ID, board);
+
+        // Create the final draft state, if necessary
+        if (finalDraftStates == null)
+        {
+            finalDraftStates = new int[SAVE_AFTER_EVERY][countries.length + 1];
+        }
+
+        // Initialize reserved territories
+        if (reservedTerrs == null)
+        {
+            reservedTerrs = new ArrayList<Integer>();
+        }
+        if (antiReservedTerrs == null)
+        {
+            antiReservedTerrs = new ArrayList<Integer>();
+        }
+        reservedTerrs.clear();
+        antiReservedTerrs.clear();
+
+        if (RESERVED_CONTINENT != -1)
+        {
+            // Grab all the territories from this continent
+            ArrayList<Integer> countriesInCont = new ArrayList<Integer>();
+            for (Country country : countries)
+            {
+                if (country.getContinent() == RESERVED_CONTINENT)
+                {
+                    countriesInCont.add((Integer) country.getCode());
+                }
+            }
+
+            // Pick the reserved ones at random
+            for (int i = 0; i < NUM_RESERVED_TERRS; ++i)
+            {
+                if (countriesInCont.isEmpty())
+                {
+                    assert(false);
+                }
+                Integer terr = countriesInCont.remove((int)(Math.random()*countriesInCont.size()));
+                reservedTerrs.add(terr);
+            }
+            while (!countriesInCont.isEmpty())
+            {
+                antiReservedTerrs.add(countriesInCont.remove(0));
+            }
+        }
+    }
+
+    /**
+     * Just pick randomly.
+     * @param draftState Who owns what
+     * @param unownedCountries The territories left to be picked
+     * @return The index of the territory to pick
+     */
+    protected int getPick(int[] draftState, ArrayList<Integer> unownedCountries)
+    {
+        int pick = -1;
+        if (ID == 0)
+        {
+            // Special reserved territories
+            if (reservedTerrs.isEmpty())
+            {
+                // Just make sure that we don't pick from the anti reserved list
+                ArrayList<Integer> availablePicks = new ArrayList<Integer>();
+                for (Integer terr : unownedCountries)
+                {
+                    if (!antiReservedTerrs.contains(terr))
+                    {
+                        availablePicks.add(terr);
+                    }
+                }
+                if (availablePicks.isEmpty())
+                {
+                    assert(false);
+                }
+                pick = availablePicks.get((int)(Math.random()*availablePicks.size()));
+            }
+            else
+            {
+                pick = reservedTerrs.remove(0);
+            }
+        }
+        else
+        {
+            // Need to check if we have to pick from the anti-reserved list
+            int maxNumPicksLeftForZero = unownedCountries.size() / board.getNumberOfPlayers() + 1;
+            int numAvailablePicksForZero = unownedCountries.size() - antiReservedTerrs.size();
+
+            if (numAvailablePicksForZero < maxNumPicksLeftForZero)
+            {
+                assert(false);
+            }
+            
+            if (numAvailablePicksForZero == maxNumPicksLeftForZero)
+            {
+                // Must choose from anti list
+                pick = antiReservedTerrs.remove((int)(Math.random()*antiReservedTerrs.size()));
+            }
+            else
+            {
+                // Pick from anywhere
+                pick = unownedCountries.get((int)(Math.random()*unownedCountries.size()));
+                if (antiReservedTerrs.contains((Integer) pick))
+                {
+                    antiReservedTerrs.remove((Integer) pick);
+                }
+            }
+        }
+
+        if (pick == -1)
+        {
+            assert(false);
+        }
+
+        finalDraftStates[numGamesPlayed % SAVE_AFTER_EVERY][pick] = ID;
+
+        return pick;
+    }
+
+    // Record the data points
+    @Override
+    public String youWon()
+    {
+        String message = super.youWon();
+
+        // Record who won
+        finalDraftStates[numGamesPlayed % SAVE_AFTER_EVERY][countries.length] = ID;
+
+        if (numGamesPlayed <= MAX_NUM_GAMES && numGamesPlayed % SAVE_AFTER_EVERY == 0)
+        {
+            String data = "";
+
+            for (int game = 0; game < SAVE_AFTER_EVERY; ++game)
+            {
+                for (int player = 0; player < board.getNumberOfPlayers(); ++player)
+                {
+                    if (RESERVED_CONTINENT != -1 && player > 0)
+                    {
+                        // We only want to record for player 0 when we reserve terrs
+                        break;
+                    }
+
+                    // Number of territories in each continent
+                    int[] numInEachContinent = new int[board.getNumberOfContinents()];
+                    for (int i = 0; i < numInEachContinent.length; ++i)
+                    {
+                        numInEachContinent[i] = 0;
+                    }
+
+                    // List of enemy neighbours
+                    ArrayList<Integer> enemyNeighbours = new ArrayList<Integer>();
+
+                    // Number of territories with an enemy neighbour
+                    int numTerrsWithEnemy = 0;
+
+                    for (int terr = 0; terr < countries.length; ++terr)
+                    {
+                        if (finalDraftStates[game][terr] != player)
+                        {
+                            // Only are concerned with territories this player
+                            // owned at the end of the draft.
+                            continue;
+                        }
+
+                        // Increment continent count
+                        numInEachContinent[countries[terr].getContinent()]++;
+
+                        // Add any enemy neighbours we haven't seen
+                        for (int neighbour : countries[terr].getAdjoiningCodeList())
+                        {
+                            if (finalDraftStates[game][neighbour] != player &&
+                                    !enemyNeighbours.contains((Integer) neighbour))
+                            {
+                                enemyNeighbours.add((Integer) neighbour);
+                            }
+                        }
+
+                        // Check if this territory has an enemy
+                        for (int neighbour : countries[terr].getAdjoiningCodeList())
+                        {
+                            if (finalDraftStates[game][neighbour] != player)
+                            {
+                                numTerrsWithEnemy++;
+                                break;
+                            }
+                        }
+                    }
+
+                    // Record the data point
+                    for (int i = 0; i < numInEachContinent.length; ++i)
+                    {
+                        data += "" + numInEachContinent[i] + ",";
+                    }
+                    data += "" + enemyNeighbours.size() + "," + numTerrsWithEnemy + ",";
+                    if (finalDraftStates[game][countries.length] == player)
+                    {
+                        // A win
+                        data += "1\n";
+                    }
+                    else
+                    {
+                        // A loss
+                        data += "0\n";
+                    }
+                }
+            }
+
+            // Finally, write the data to file if it is time to do so
+            try
+            {
+                PrintWriter out = new PrintWriter(new FileWriter(DATA_FILENAME, true));
+                out.print(data);
+                out.close();
+                data = "";
+            }
+            catch (Exception e)
+            {
+                assert(false);
+            }
+        }
+
+        System.out.println("\n\n\n\nHave now played " + numGamesPlayed + " games\n\n\n\n");
+
+        return message;
+    }
+}
